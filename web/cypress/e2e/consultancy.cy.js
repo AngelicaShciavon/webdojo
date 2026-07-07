7a20da3e55563f93b5a41e3875a40d1dd50aba7f
usuario123 <angelica@obi.tec.br>
2026-07-06 23:07:00 -0300

    UPDATE: Added a new test case for consultancy page in Cypress.

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -3,6 +3,11 @@ describe('Formulário de consultoria', () => {
     cy.START()
     cy.submetLoginForm('papito@webdojo.com', 'katana123')
     cy.goTo('s', 'Consultoria')
+    cy.get('#name').type('Fernando Papito')
+    cy.get('#email').type('papito@teste.com.br')
+    cy.get('#phone').type('(00) 0000-0000').should('have.value', '(00) 0000-0000')
+    cy.get('#consultancyType').select('inCompany').should('have.value', 'inCompany')
+     
   })
 })
 
